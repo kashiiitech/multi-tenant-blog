3cb0b2c54d58eafb285dc41affc6b8f90881c39d
kashiiitech <kashiiitech@gmail.com>
2025-06-29 21:25:28 +0500

    added clerk athentication and removed default nextjs code

---
+++ app/components/nav.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import * as React from 'react';
+
+
+const Nav: React.FC = () => {
+    return (
+        <nav>
+            <div>
+                <h1>Blog Application</h1>
+            </div>
+            <div></div>
+        </nav>
+    )
+}
+
+export default Nav;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <ClerkProvider>
+        <SignedOut>
+          <div className="min-w-screen min-h-screen flex justify-center items-center">
+            <SignIn routing="hash">  
+            </SignIn>
+          </div>
+        </SignedOut>
+        <SignedIn>
+          {children}
+        </SignedIn>
+      </ClerkProvider>
       </body>
     </html>
   );
